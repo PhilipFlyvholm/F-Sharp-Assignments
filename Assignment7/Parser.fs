01afb08a1016e25b7b8bcebb2e05d338d72c6f72
Philip Kristian Møller Flyvholm <phif@itu.dk>
2022-03-16 10:14:05 +0100

    Init of assignment7

---
+++ Assignment7/Parser.fs
@@ -0,0 +1,105 @@
+﻿module ImpParser
+
+    open Eval
+
+    (*
+
+    The interfaces for JParsec and FParsecLight are identical and the implementations should always produce the same output
+    for successful parses although running times and error messages will differ. Please report any inconsistencies.
+
+    *)
+
+    open JParsec.TextParser             // Example parser combinator library. Use for CodeJudge.
+    // open FParsecLight.TextParser     // Industrial parser-combinator library. Use for Scrabble Project.
+    
+    let pIntToChar  = pstring "intToChar"
+    let pPointValue = pstring "pointValue"
+
+    let pCharToInt  = pstring "charToInt"
+    let pToUpper    = pstring "toUpper"
+    let pToLower    = pstring "toLower"
+    let pCharValue  = pstring "charValue"
+
+    let pTrue       = pstring "true"
+    let pFalse      = pstring "false"
+    let pIsDigit    = pstring "IsDigit"
+    let pIsLetter   = pstring "IsLetter"
+    let pIsVowel   = pstring "IsVowel"
+
+    let pif       = pstring "if"
+    let pthen     = pstring "then"
+    let pelse     = pstring "else"
+    let pwhile    = pstring "while"
+    let pdo       = pstring "do"
+    let pdeclare  = pstring "not implemented"
+
+    let whitespaceChar = satisfy System.Char.IsWhiteSpace
+    let pletter        = satisfy System.Char.IsLetter
+    let palphanumeric  = satisfy System.Char.IsLetterOrDigit
+
+    let spaces         = pstring "not implemented"
+    let spaces1        = pstring "not implemented"
+
+    let (.>*>.) _ _ = failwith "not implemented"
+    let (.>*>) _ _  = failwith "not implemented"
+    let (>*>.) _ _  = failwith "not implemented"
+
+    let parenthesise p = p // incorrect (not implemented)
+
+    let pid = pstring "not implemented"
+
+    
+    let unop _ = failwith "not implemented"
+    let binop _ p1 p2 = p1 .>>. p2 // incorrect (not implemented)
+
+    let TermParse, tref = createParserForwardedToRef<aExp>()
+    let ProdParse, pref = createParserForwardedToRef<aExp>()
+    let AtomParse, aref = createParserForwardedToRef<aExp>()
+
+    let AddParse = binop (pchar '+') ProdParse TermParse |>> Add <?> "Add"
+    do tref := choice [AddParse; ProdParse]
+
+    let MulParse = binop (pchar '*') AtomParse ProdParse |>> Mul <?> "Mul"
+    do pref := choice [MulParse; AtomParse]
+
+    let NParse   = pint32 |>> N <?> "Int"
+    let ParParse = parenthesise TermParse
+    do aref := choice [NParse; ParParse]
+
+    let AexpParse = TermParse 
+
+    let CexpParse = pstring "not implemented"
+
+    let BexpParse = pstring "not implemented"
+
+    let stmntParse = pstring "not implemented"
+
+(* These five types will move out of this file once you start working on the project *)
+    type coord      = int * int
+    type squareProg = Map<int, string>
+    type boardProg  = {
+            prog       : string;
+            squares    : Map<int, squareProg>
+            usedSquare : int
+            center     : coord
+    
+            isInfinite : bool   // For pretty-printing purposes only
+            ppSquare   : string // For pretty-printing purposes only
+        }
+
+    type word   = (char * int) list
+    type square = Map<int, squareFun>
+
+    let parseSquareProg _ = failwith "not implemented"
+
+    let parseBoardProg _ = failwith "not implemented"
+
+    type boardFun2 = coord -> StateMonad.Result<square option, StateMonad.Error>
+    type board = {
+        center        : coord
+        defaultSquare : square
+        squares       : boardFun2
+    }
+
+    let mkBoard (bp : boardProg) = failwith "not implemented"
+
